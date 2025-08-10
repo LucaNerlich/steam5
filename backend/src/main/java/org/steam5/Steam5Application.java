4416a2ab5e7bf3162e80dba167a0073de587445e
Luca Nerlich <nerlich@adobe.com>
2025-08-10 11:39:14 +0200

    initial backend commit

---
+++ backend/src/main/java/org/steam5/Steam5Application.java
@@ -0,0 +1,13 @@
+package org.steam5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Steam5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Steam5Application.class, args);
+    }
+
+}
