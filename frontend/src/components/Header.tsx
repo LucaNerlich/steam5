8735c173323c0e4e329a4caee28b5dd31f7bef7b
Luca Nerlich <nerlich@adobe.com>
2025-08-26 16:53:16 +0200

    update icons
    thanks til!

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,6 +6,8 @@ import {Routes} from "../../app/routes";
 import "@/styles/components/header.css";
 import SteamLoginButton from "@/components/SteamLoginButton";
 import {usePathname} from "next/navigation";
+import IconImage from "../../public/icon.svg"
+import IconFullImage from "../../public/icon-full.svg"
 
 export default function Header() {
     const pathname = usePathname();
@@ -14,8 +16,22 @@ export default function Header() {
         <header>
             <div className="container">
                 <Link href={Routes.home} className="brand">
-                    <Image src="/logo.svg" alt="Steam5" width={20} height={20} priority/>
-                    <span>Steam5</span>
+                    <Image
+                        src={IconFullImage}
+                        alt="Steam5"
+                        width={80}
+                        height={30}
+                        priority
+                        className="brand__logo-full"
+                    />
+                    <Image
+                        src={IconImage}
+                        alt=""
+                        width={20}
+                        height={20}
+                        priority
+                        className="brand__logo-icon"
+                    />
                 </Link>
                 {underReviewGuesser && (
                     <span className="game-title" aria-hidden="true">
