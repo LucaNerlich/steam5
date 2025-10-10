5e37b38c67d5c44164305bc22db6932203ec13f8
Luca Nerlich <luca.nerlich@gmail.com>
2025-10-10 18:29:00 +0200

    next 16

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,12 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
+    reactCompiler: true,
     experimental: {
-        reactCompiler: true,
+        turbopackFileSystemCacheForDev: true,
     },
     images: {
         remotePatterns: [
