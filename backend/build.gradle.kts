6b05e7ac133edebd292aff538c7c9f0b9f2823b3
Luca Nerlich <nerlich@adobe.com>
2026-01-17 10:53:17 +0100

    spring boot 4.0.1

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.0"
+    id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.asciidoctor.jvm.convert") version "4.0.5"
 }
 
 group = "org.steam5"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 java {
     toolchain {
